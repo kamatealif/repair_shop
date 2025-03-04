8acb769ea3b28082de116773c84924cca6f44712
kamatealif <kamatealip1@gmail.com>
2025-03-04 20:04:36 +0530

    Add metadata for pages and create a custom 404 not found page

--- app/(rs)/home/page.tsx
+++ app/(rs)/home/page.tsx
@@ -1,3 +1,8 @@
+
+export const metadata ={
+    title: "Home"
+}
+
 export default function Home(){
     return(
         <div>
---
+++ app/not-found.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+
+
+export const metadata = {
+    title:'Page Not Found',
+}
+ 
+export default function NotFound() {
+  return (
+    <div className='px-2 w-full '>
+        <div className='mx-auto py-4 flex flex-col justify-center items-center gap-4'>
+
+        
+      <h2 className='text-2xl'>Page Not Found</h2>
+      <Image
+        src="/images/404-image.png"
+        width={300}
+        height={300}
+        alt="Page Not found"
+        priority={true}
+        title='Page Not Found'
+        className='m-0 rounded-xl'
+        />
+        </div>
+    </div>
+  )
+}
